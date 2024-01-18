2cfe3576434b43e96dd5e2a4f4d430f80a823ee2
Gabriela <gabrielaap.nunez@gmail.com>
2024-01-18 11:41:26 -0300

    Correção e adaptação da função do exercício 3 para melhor legibilidade

--- Exercício 3/3.py
+++ Exercício 3/3.py
@@ -5,10 +5,10 @@ transforme em metros,centímetros e milímetros.
 
 user_input = int(input("Digite um valor em quilômetros: "))
 
-def transform_to_mm_cm(km):
-    mm = km * 1000
-    cm = km * 100
-    print(f'O valor inserido corresponde a {mm} milímetros!')
-    print(f'O valor inserido corresponde a {cm} centímetros!')
+def transform_to_m_mm_cm(km):
+    m = km * 1000 # Multiplicamos o valor de km por mil para obter o valor em metros,
+    cm = km * 100000 # por cem mil para obter o valor em centímetros,
+    mm = km * 1000000 # e por um milhão para obter o valor em milímetros.
+    print(f'O valor inserido corresponde a {m} metros, {cm} centímetros e {mm} milímetros!')
 
-transform_to_mm_cm(user_input)
+transform_to_m_mm_cm(user_input)
