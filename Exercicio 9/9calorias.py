a795a555c0cd1787ffbc85dc5f7b8e4d090f9fc5
Thayna Tolentino <tatarlt7@gmail.com>
2024-01-18 11:27:39 -0300

    Exercicio9

---
+++ Exercicio 9/9calorias.py
@@ -0,0 +1,13 @@
+# 9. Solicite ao usuário o número de horas de exercício físico por semana.
+#Calcule o total de calorias queimadas em um mês, considerando uma média de 5 calorias por minuto de exercício.
+
+horas_semana = float(input('Digite o número de horas de exercício físico por semana:'))
+
+MES = 30 #Atribuindo que um mês contém 30 dias
+
+horas_em_minutos = horas_semana*60 #Transformando as horas em minutos
+calorias_gasta = 5*horas_em_minutos*MES
+
+
+print(f'O total de calorias gastas no mês foi de {calorias_gasta}')
+
