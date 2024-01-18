9095c8fe01cc44df4cfe48f75afcbd3d0b9f1e6c
Renatarafaelaalves <renatarafaela85@yahoo.com.br>
2024-01-18 19:30:55 -0300

    Excercicio 01 resolvido

---
+++ Tomada de Decisão e Estruturas de Repetição/Exercicio 1/1-imprima-o maior.py
@@ -0,0 +1,9 @@
+# 1 Faça um Programa quepeça dois números e imprima o maior deles
+
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite um numero: '))
+
+if num1 > num2:
+    print(f'Numero {num1} é maior.')
+else:
+    print(f'Numero {num2} é maior.')
