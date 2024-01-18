9e15b21d3c7e8ab0316482ea502d6a6906e45b38
Renatarafaelaalves <renatarafaela85@yahoo.com.br>
2024-01-17 21:14:31 -0300

    Excercio 1 resolvido!

---
+++ Exercicio 1/1.py
@@ -0,0 +1,17 @@
+# 1  . Faça um Programa que peça dois números, realize as principais
+# operações soma, subtração, multiplicação, divisão
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+soma = numero1+numero2
+print(f"Resultado da Soma: {soma}")
+
+subtracao = numero1-numero2
+print(f"Resultado da Subtração: {subtracao}")
+
+multiplicacao = numero1*numero2
+print(f"Resultado da Multiplicação: {multiplicacao}")
+
+divisao =  numero1//numero2
+print(f"Resultado da Divisão: {divisao}")
