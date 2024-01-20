0f6eb342e430527a75aec6a1fa191f9a92db0a3d
Renatarafaelaalves <renatarafaela85@yahoo.com.br>
2024-01-20 19:22:52 -0300

    Excercicio 01 resolvido

---
+++ Funções/Exercicio 1/tres-argumento.py
@@ -0,0 +1,10 @@
+num1 = int(input('Digite o primeiro numero: '))
+num2 = int(input('Digite o segundo numero: '))
+num3 = int(input('Digite o terceiro numero: '))
+
+def soma(num1, num2, num3):
+   
+    calculo = num1+num2+num3
+    print(f'O resultado da soma é {calculo}')
+
+soma(num1, num2, num3)
